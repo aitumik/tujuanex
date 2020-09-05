c4a25a7ae058495ba573c5226339192e59c80ecd
Nathan <nathanpykimutai@gmail.com>
2020-09-06 02:35:29 +0300

    its been long since I worked on this project

--- app/tujuanex/models.py
+++ app/tujuanex/models.py
@@ -13,6 +13,7 @@ class Role(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(64),unique=True)
     default = db.Column(db.Boolean,default=False,index=True)
+    description = db.Column(db.String(100))
 
     users = db.relationship('User',backref='role',lazy='dynamic')
 
@@ -82,4 +83,7 @@ class User(db.Model):
 
 class Likes(db.Model):
     __tablename__ = "likes"
+
     id = db.Column(db.Integer,primary_key=True)
+    count = db.Column(db.Integer,default=0)
+
